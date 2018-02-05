3245815b8dd850624e542e94d51aab76479a72d5
Atilim Gunes Baydin <atilimgunes.baydin@gmail.com>
2018-02-05 16:44:06 +0000

    VGG

--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@ python train.py --cuda --model mlp --method sgdn_hd --save --epochs 100 --beta 0
 python train.py --cuda --model mlp --method adam    --save --epochs 100 --beta 1e-7
 python train.py --cuda --model mlp --method adam_hd --save --epochs 100 --beta 1e-7
 
-python train.py --cuda --model mlp --method sgd     --save --epochs 200 --beta 0.001
-python train.py --cuda --model mlp --method sgd_hd  --save --epochs 200 --beta 0.001
-python train.py --cuda --model mlp --method sgdn    --save --epochs 200 --beta 0.001
-python train.py --cuda --model mlp --method sgdn_hd --save --epochs 200 --beta 0.001
-python train.py --cuda --model mlp --method adam    --save --epochs 200 --beta 1e-8
-python train.py --cuda --model mlp --method adam_hd --save --epochs 200 --beta 1e-8
+python train.py --cuda --model vgg --method sgd     --save --epochs 200 --beta 0.001
+python train.py --cuda --model vgg --method sgd_hd  --save --epochs 200 --beta 0.001
+python train.py --cuda --model vgg --method sgdn    --save --epochs 200 --beta 0.001
+python train.py --cuda --model vgg --method sgdn_hd --save --epochs 200 --beta 0.001
+python train.py --cuda --model vgg --method adam    --save --epochs 200 --beta 1e-8
+python train.py --cuda --model vgg --method adam_hd --save --epochs 200 --beta 1e-8
